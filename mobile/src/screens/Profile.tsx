e6df603af9ebd00412f42f99de6e500904924a73
이정철 <arjungtaeng@me.com>
2026-05-26 11:06:06 +0900

    chore: Bump app version to 251

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -193,7 +193,7 @@ export default function ProfileScreen() {
 
         {/* Credits */}
         <Text style={[styles.credits, { color: T.textFaint, fontFamily: T.mono }]}>
-          EngCat v250
+          EngCat v251
         </Text>
       </ScrollView>
     </SafeAreaView>
